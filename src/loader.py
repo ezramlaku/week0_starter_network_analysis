01efe7d4d89d32ebff1d959e0bf2c200ae436e91
EzraMelaku <45677653+ezramlaku@users.noreply.github.com>
2023-11-29 21:31:45 +0300

    Update loader.py

--- src/loader.py
+++ src/loader.py
@@ -5,8 +5,9 @@ import io
 import shutil
 import copy
 from datetime import datetime
-# from pick import pick
+from pick import pick
 from time import sleep
+
 
 
 # Create wrapper classes for using slack_sdk in place of slacker
@@ -21,20 +22,20 @@ class SlackDataLoader:
     You'll see reference files for different kinds of conversations: 
     users.json files for all types of users that exist in the slack workspace
     channels.json files for public channels, 
-
+    
     These files contain metadata about the conversations, including their names and IDs.
 
     For secruity reason, we have annonymized names - the names you will see are generated using faker library.
-
+    
     '''
-
     def __init__(self, path):
         '''
         path: path to the slack exported data folder
         '''
         self.path = path
         self.channels = self.get_channels()
-        self.users = self.get_users()
+        self.users = self.get_ussers()
+    
 
     def get_users(self):
         '''
@@ -44,7 +45,7 @@ class SlackDataLoader:
             users = json.load(f)
 
         return users
-
+    
     def get_channels(self):
         '''
         write a function to get all the channels from the json file
@@ -57,10 +58,10 @@ class SlackDataLoader:
     def get_channel_messages(self, channel_name):
         '''
         write a function to get all the messages from a channel
-
+        
         '''
 
-    #
+    # 
     def get_user_map(self):
         '''
         write a function to get a map between user id and user name
@@ -70,11 +71,14 @@ class SlackDataLoader:
         for user in self.users:
             userNamesById[user['id']] = user['name']
             userIdsByName[user['name']] = user['id']
-        return userNamesById, userIdsByName
+        return userNamesById, userIdsByName        
+
+
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Export Slack history')
 
+    
     parser.add_argument('--zip', help="Name of a zip file to import")
     args = parser.parse_args()
